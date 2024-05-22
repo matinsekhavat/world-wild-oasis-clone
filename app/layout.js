4d6bcc1848dc727cc582f38a1c5372ab19a9092f
matinsekhavat <matinhyphen@gmail.com>
2024-05-22 18:06:03 +0330

    layout css code part-1

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-import "./_styles/globals.css";
+// import "./_styles/globals.css";
+import "@/app/_styles/globals.css";
 import Logo from "./_components/Logo";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
         </header>
