f4fa8cce22011f5b0d26c770fa05256c359256fe
matinsekhavat <matinhyphen@gmail.com>
2024-05-24 00:25:44 +0330

    add simple static name to guestArea(page: account)

--- app/account/page.js
+++ app/account/page.js
@@ -3,7 +3,11 @@ export const metadata = {
   title: "geust area",
 };
 function Account() {
-  return <div>Account</div>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome Matin
+    </h2>
+  );
 }
 
 export default Account;
