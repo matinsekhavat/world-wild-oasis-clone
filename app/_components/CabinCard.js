9656e3f17afe4c990dc8f575ff27969efeca644b
matinsekhavat <matinhyphen@gmail.com>
2024-05-24 00:24:10 +0330

    create and implement CabinCard Component

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -1,4 +1,5 @@
 import { UsersIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 
 function CabinCard({ cabin }) {
   const { id, name, maxCapacity, regularPrice, discount, image } = cabin;
@@ -42,12 +43,12 @@ function CabinCard({ cabin }) {
         </div>
 
         <div className="bg-primary-950 border-t border-t-primary-800 text-right">
-          <a
+          <Link
             href={`/cabins/${id}`}
             className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
           >
             Details & reservation &rarr;
-          </a>
+          </Link>
         </div>
       </div>
     </div>
