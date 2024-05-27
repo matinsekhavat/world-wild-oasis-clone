5a916fb1feb819c33bc448cdd6dcb50d67e61e66
matinsekhavat <matinhyphen@gmail.com>
2024-05-27 14:51:26 +0330

    add TextExpander component to use in dynamic cabinId segment for decription

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,7 +1,7 @@
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
-
+import TextExpander from "@/app/_components/TextExpander";
 //set StaticParams
 export async function generateStaticParams() {
   const cabins = await getCabins();
@@ -48,7 +48,9 @@ export default async function Page({ params, searchParams }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
