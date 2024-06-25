c08d85382e1786d2280357cecba5a9fbec63b8f2
matinsekhavat <matinhyphen@gmail.com>
2024-06-26 01:45:59 +0330

    make some capacity filtering logic in CabinList.js

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,13 +1,24 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
-export default async function CabinList() {
+export default async function CabinList({ filter }) {
   const cabins = await getCabins();
   if (!cabins.length) {
     return null;
   }
+
+  let displayedCabins = [];
+  if (filter === "all") displayedCabins = cabins;
+  if (filter === "small")
+    displayedCabins = cabins.filter((item) => item.maxCapacity <= 3);
+  if (filter === "medium")
+    displayedCabins = cabins.filter(
+      (item) => item.maxCapacity >= 4 && item.maxCapacity <= 7
+    );
+  if (filter === "large")
+    displayedCabins = cabins.filter((item) => item.maxCapacity >= 8);
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins?.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
