d2d7e5f7d895480219715ead0aca3668173dc29c
matinsekhavat <matinhyphen@gmail.com>
2024-06-26 02:04:02 +0330

    implement onCLick feature for push searchParams into url

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ function page({ params, searchParams }) {
   //only available into main page.js  file not in any server component file
 }
 ```
+
+for searchParms follow this instructure
+
+```javascript
+const searchParams = useSearchParams();
+const router = useRouter();
+const pathname = usePathname();
+function handleFilter(value) {
+  const params = new URLSearchParams(searchParams);
+  params.set("capacity", value);
+  console.log(params.toString());
+  router.replace(`${pathname}?${params.toString()}`);
+}
+```
--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,16 +1,42 @@
 "use client";
 
+import { usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 function Filter() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+  function handleFilter(value) {
+    const params = new URLSearchParams(searchParams);
+    params.set("capacity", value);
+    console.log(params.toString());
+    router.replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <div className="border border-primary-800 flex">
-      <button className="px-5 py-2 hover:bg-primary-700">All cabins</button>
-      <button className="px-5 py-2 hover:bg-primary-700">
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("all")}
+      >
+        All cabins
+      </button>
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("small")}
+      >
         1&mdash; 3 guest
       </button>{" "}
-      <button className="px-5 py-2 hover:bg-primary-700">
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("medium")}
+      >
         4&mdash; 7 guest
       </button>
-      <button className="px-5 py-2 hover:bg-primary-700">
+      <button
+        className="px-5 py-2 hover:bg-primary-700"
+        onClick={() => handleFilter("large")}
+      >
         8&mdash; 12 guest
       </button>
     </div>
