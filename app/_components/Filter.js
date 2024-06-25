2ab0e1d95189edaadae3f6e771858c4a7996728f
matinsekhavat <matinhyphen@gmail.com>
2024-06-26 01:45:05 +0330

    create firsy ui of filter component for using that inside cabins page

---
+++ app/_components/Filter.js
@@ -0,0 +1,20 @@
+"use client";
+
+function Filter() {
+  return (
+    <div className="border border-primary-800 flex">
+      <button className="px-5 py-2 hover:bg-primary-700">All cabins</button>
+      <button className="px-5 py-2 hover:bg-primary-700">
+        1&mdash; 3 guest
+      </button>{" "}
+      <button className="px-5 py-2 hover:bg-primary-700">
+        4&mdash; 7 guest
+      </button>
+      <button className="px-5 py-2 hover:bg-primary-700">
+        8&mdash; 12 guest
+      </button>
+    </div>
+  );
+}
+
+export default Filter;
